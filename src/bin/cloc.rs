ee32c953371d7c1c811a4980017e9f29de59f15a
ltoddy <taoliu0509@gmail.com>
2020-03-02 20:58:07 +0800

    (WIP) add output format

--- src/bin/cloc.rs
+++ src/bin/cloc.rs
@@ -7,12 +7,11 @@ use cloc::pprint::PrettyPrinter;
 
 fn main() {
     let opt: Options = Options::from_args();
-
-    let Options { entry } = opt;
+    let Options { entry, output } = opt;
 
     let engine = Engine::new(entry);
     let details = engine.calculate();
-
     let total = TotalDetail::from_details(details);
+
     PrettyPrinter::terminal(total);
 }
--- src/options.rs
+++ src/options.rs
@@ -1,10 +1,41 @@
 use std::path::PathBuf;
+use std::str::FromStr;
 
 use structopt::StructOpt;
+
+use crate::error::ClocError;
+
+#[derive(Debug)]
+pub enum Output {
+    Terminal,
+}
+
+impl FromStr for Output {
+    type Err = ClocError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Terminal" => Ok(Output::Terminal),
+            _ => todo!(),
+        }
+    }
+}
+
+impl ToString for Output {
+    fn to_string(&self) -> String {
+        match self {
+            Output::Terminal => String::from("Terminal"),
+        }
+    }
+}
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "cloc - Count, or compute differences of, lines of source code and comments.")]
 pub struct Options {
+    // TODO: output format
+    #[structopt(short = "o", long = "output", default_value = "Terminal")]
+    pub output: Output,
+
     #[structopt(name = "path", parse(from_os_str))]
     pub entry: PathBuf,
 }
