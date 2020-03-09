6230a97d8b44fad51ab1ff8ba933117b725c499e
ltoddy <taoliu0509@gmail.com>
2020-03-09 20:46:07 +0800

    update project description

--- src/error.rs
+++ src/error.rs
@@ -3,13 +3,13 @@ use std::io::Error;
 
 #[derive(Debug)]
 pub enum ClocError {
-    // 未识别的文件, 可以识别的文件记录在`src/config.rs`的Config结构体中
+    /// 未识别的文件, 可以识别的文件记录在`src/config.rs`的Config结构体中
     Unrecognized,
-    // 非文本文件, 例如二进制文件不做统计
+    /// 非文本文件, 例如二进制文件不做统计
     NonTextFile,
-    // Io异常
+    /// Io异常
     Io(std::io::Error),
-    // 运行时的命令行参数的输入了无效参数
+    /// 运行时的命令行参数的输入了无效参数
     InvalidCommandArgs,
 }
 
@@ -17,13 +17,7 @@ impl std::error::Error for ClocError {}
 
 impl fmt::Display for ClocError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        // TODO
-        match self {
-            ClocError::Unrecognized => write!(f, "Unrecognized"),
-            ClocError::NonTextFile => write!(f, "NonTextFile"),
-            ClocError::Io(_) => write!(f, "Io"),
-            InvalidCommandArgs => write!(f, "Invalid command args"),
-        }
+        fmt::Debug::fmt(&self, f)
     }
 }
 
